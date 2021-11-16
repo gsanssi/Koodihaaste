dfc7b1f97220fff5472227bc94b781ac356afd9a
gsanssi <91940391+gsanssi@users.noreply.github.com>
2021-11-16 12:46:03 +0200

    Initial commit

---
+++ src/kahvikone.py
@@ -0,0 +1,6 @@
+from typing import Any
+
+
+def kahvikone(kahvin_hinta: float, syotetyt_rahat: float) -> Any:
+    pass
+    #TODO
---
+++ src/tennistilastot.py
@@ -0,0 +1,22 @@
+
+
+class TournamentStats:
+    def __init__(self, tournament_data_filepath: str, match_data_filepath):
+        # TODO: Process data from csv into any format for analyzing.
+        pass
+
+    def tournaments_by_year(self):
+        # TODO print amount of tournaments by year.
+        pass
+
+    def tournaments_won_by_player(self):
+        # TODO print playername and amount of tournaments won in descending order by amount of tournaments they won
+        # in 2010=2019
+        pass
+
+    def print_bracket_by_tournament(self, tourney_year_id: str):
+        """
+        :param tourney_year_id: 2010-339
+        """
+    # TODO Define other functions here.
+
